0309c41d3b9aa332b670abcf1d0a5c0634d8ef52
Jim Gorman <jgorman30@gatech.edu>
2018-07-17 08:47:48 -0400

    wip StudentLessonAnswerService

---
+++ MetacognitiveTutor.Api/Services/StudentLessonAnswerService.cs
@@ -0,0 +1,12 @@
+﻿using MetacognitiveTutor.DataLayer.Repositories;
+using ServiceStack.ServiceInterface;
+
+namespace MetacognitiveTutor.Api.Services
+{
+    public class StudentLessonAnswerService : Service
+    {
+        public UserRepository UserRepository { get; set; }
+        public LessonRepository LessonRepository { get; set; }
+        public StudentLessonAnswerRepository StudentLessonAnswerRepository { get; set; }
+    }
+}
