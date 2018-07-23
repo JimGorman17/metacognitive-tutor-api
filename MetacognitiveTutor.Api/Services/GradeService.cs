0424d2f2452197bcaa356bb8debd7bcab292c14a
Jim Gorman <jgorman30@gatech.edu>
2018-07-23 07:50:45 -0400

    Implemented LessonDeleteRequest Api method.

--- MetacognitiveTutor.Api/Services/GradeService.cs
+++ MetacognitiveTutor.Api/Services/GradeService.cs
@@ -1,4 +1,9 @@
-﻿using MetacognitiveTutor.DataLayer.Repositories;
+﻿using System.Net;
+using MetacognitiveTutor.Api.Helpers;
+using MetacognitiveTutor.Api.Interfaces;
+using MetacognitiveTutor.DataLayer.Repositories;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceHost;
 using ServiceStack.ServiceInterface;
 
 namespace MetacognitiveTutor.Api.Services
@@ -7,5 +12,61 @@ namespace MetacognitiveTutor.Api.Services
     {
         public UserRepository UserRepository { get; set; }
         public GradeRepository GradeRepository { get; set; }
+
+        [Route("/grade/upsert", "POST")]
+        public class GradeUpsertRequest : IProviderRequest, IReturnVoid
+        {
+            [ApiMember(IsRequired = true)] public int LessonId { get; set; }
+            [ApiMember(IsRequired = true)] public string StudentProvider { get; set; }
+            [ApiMember(IsRequired = true)] public string StudentProviderId { get; set; }
+            [ApiMember(IsRequired = true, Description = "Refers to the teacher submitting the grade.")] public string Provider { get; set; }
+            [ApiMember(IsRequired = true, Description = "Refers to the teacher submitting the grade.")] public string ProviderId { get; set; }
+            [ApiMember(IsRequired = true)] public string Grade { get; set; }
+            public string Comments { get; set; }
+        }
+
+        [Route("/grade/delete", "DELETE")]
+        public class LessonDeleteRequest : IProviderRequest, IReturnVoid
+        {
+            [ApiMember(IsRequired = true)] public int LessonId { get; set; }
+            [ApiMember(IsRequired = true)] public string StudentProvider { get; set; }
+            [ApiMember(IsRequired = true)] public string StudentProviderId { get; set; }
+            [ApiMember(IsRequired = true, Description = "Refers to the teacher submitting the grade.")] public string Provider { get; set; }
+            [ApiMember(IsRequired = true, Description = "Refers to the teacher submitting the grade.")] public string ProviderId { get; set; }
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public void Delete(LessonDeleteRequest request)
+        {
+            Guard.IsTrue(lessonId => 0 < lessonId, request.LessonId);
+            Guard.AgainstEmpty(request.Provider);
+            Guard.AgainstEmpty(request.ProviderId);
+            var existingUser = UserHelpers.GetExistingUser(request, UserRepository);
+            Guard.IsTrue(eu => eu.IsNew == false, existingUser);
+
+            Guard.AgainstEmpty(request.StudentProvider);
+            Guard.AgainstEmpty(request.StudentProviderId);
+            var existingStudent = UserHelpers.GetExistingUser(new LessonDeleteRequest {Provider = request.StudentProvider, ProviderId = request.StudentProviderId }, UserRepository);
+            Guard.IsTrue(eu => eu.IsNew == false, existingStudent);
+            Guard.IsTrue(es => es.IsStudent, existingStudent);
+
+            var grade = GradeRepository.GetGrade(request.LessonId, request.StudentProvider, request.ProviderId);
+            if (grade == null)
+            {
+                return;
+            }
+
+            if (existingUser.IsTeacher == false || request.Provider != grade.TeacherProvider || request.ProviderId != grade.TeacherProviderId)
+            {
+                throw new HttpError(HttpStatusCode.Unauthorized, "Unauthorized");
+            }
+
+            if (grade.IsDeleted)
+            {
+                return;
+            }
+
+            GradeRepository.DeleteGrade(grade.Id);
+        }
     }
 }
--- MetacognitiveTutor.DataLayer/Repositories/GradeRepository.cs
+++ MetacognitiveTutor.DataLayer/Repositories/GradeRepository.cs
@@ -1,4 +1,5 @@
-﻿using MetacognitiveTutor.DataLayer.Models;
+﻿using System;
+using MetacognitiveTutor.DataLayer.Models;
 
 namespace MetacognitiveTutor.DataLayer.Repositories
 {
@@ -7,6 +8,11 @@ namespace MetacognitiveTutor.DataLayer.Repositories
         public GradeModel GetGrade(int lessonId, string studentProvider, string studentProviderId)
         {
             return Database.SingleOrDefault<GradeModel>("WHERE (IsDeleted = @0) AND (LessonId = @1) AND (StudentProvider = @2) AND (StudentProviderId = @3)", false, lessonId, studentProvider, studentProviderId);
+        }
+
+        public void DeleteGrade(int gradeId)
+        {
+            Database.Update<GradeModel>("SET [UpdateDateUtc]=@0, IsDeleted=@1 WHERE (Id = @2)", DateTime.UtcNow, true, gradeId);
         }
     }
 }
